e5b97e3baf1c814a709b22dc3482d1bb918ca7a9
kellyh06 <kelly.hurt@tptlive.ee>
2024-11-12 14:15:27 +0200

    Add greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-function Hello() {
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello World</p>
+      <p>Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
+      <Hello name='George' age={26+10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
